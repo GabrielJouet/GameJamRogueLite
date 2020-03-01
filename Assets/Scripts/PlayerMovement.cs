d38229c99ceae2e6c7fd8a35b29e804a3b76d4fe
DULEU LUCAS <lucas.duleu@viacesi.fr>
2020-03-01 17:09:27 +0100

    [Code][Scene] Basic Lobby Player Movement / Base Scene created
    - Added a basic player movement script
    - Created the Scene of the Base

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if (horizontalInput != 0)
+        {
+            transform.Translate(Vector2.right * horizontalInput * Time.deltaTime * _speed);
+        }
+        if (verticalInput != 0)
+        {
+            transform.Translate(Vector2.up * verticalInput * Time.deltaTime * _speed);
+        }
+    }
+}
