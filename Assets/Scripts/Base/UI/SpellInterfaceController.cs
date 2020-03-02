92f813d03a38ba7a8557c8c44b8857bf901e2095
DULEU LUCAS <lucas.duleu@viacesi.fr>
2020-03-02 13:30:57 +0100

    [UI]
    - Added a script and asset to be able to  select one spell in particular

---
+++ Assets/Scripts/Base/UI/SpellInterfaceController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellInterfaceController : MonoBehaviour
+{
+
+    private int _actualSpellId = 0;
+
+    [SerializeField]
+    private SpellSelector[] spellSelector;
+
+    [SerializeField]
+    private GameObject[] selector;
+
+    public void ChangeSelectedSpell(int spellId)
+    {
+        if (spellId != _actualSpellId)
+        {
+            selector[spellId].SetActive(true);
+            selector[_actualSpellId].SetActive(false);
+            _actualSpellId = spellId;
+        }
+      
+    }
+}
---
+++ Assets/Scripts/Base/UI/SpellSelector.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellSelector : MonoBehaviour
+{
+    [SerializeField]
+    private int _spellId;
+    private int _actualSpellId = 0;
+    [SerializeField]
+    private SpellInterfaceController _spellInterfaceController;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            switch (_spellId)
+            {
+                case 0:
+                    _spellInterfaceController.ChangeSelectedSpell(_spellId);
+                    break;
+                case 1:
+                    _spellInterfaceController.ChangeSelectedSpell(_spellId);
+                    break;
+                case 2:
+                    _spellInterfaceController.ChangeSelectedSpell(_spellId);
+                    break;
+                case 3:
+                    _spellInterfaceController.ChangeSelectedSpell(_spellId);
+                    break;
+            }
+        }
+    }
+}
