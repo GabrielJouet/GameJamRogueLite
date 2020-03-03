7c32e99774bca9b076ec5901b7ffafc5cc03fed9
Gabriel Jouet <gjouet@studioysinteractive.com>
2020-03-03 13:43:20 +0100

    [Code] Added a basic transition saver object

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -1,21 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     [Header("Components")]
     [SerializeField]
     private CreationLevel _creationLevel;
-    
+
+
+    [Header("Prefabs")]
+    [SerializeField]
+    private GameObject _transitionSaverPrefab;
+
+
+    private TransitionSaver _transitionSaver;
 
 
     //-------------------------------Unity Methods
     private void Start()
     {
+        _transitionSaver = FindObjectOfType<TransitionSaver>();
+        if (_transitionSaver == null)
+            _transitionSaver = Instantiate(_transitionSaverPrefab).GetComponent<TransitionSaver>();
+
+        Debug.Log(_transitionSaver.GetScrapCount());
+        _transitionSaver.SetScrapCount(50);
+
         int rng = System.Environment.TickCount;
         Random.InitState(rng);
         Debug.Log(rng);
 
         //When we launch the game we create a new level
         _creationLevel.CreateLevel();
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+            SceneManager.LoadScene("SampleScene");
     }
 }
---
+++ Assets/Scripts/TransitionSaver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionSaver : MonoBehaviour
+{
+    [SerializeField]
+    private int _scrapCount;
+
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    public int GetScrapCount() { return _scrapCount; }
+
+    public void AddScrapCount (int other) { _scrapCount += other; }
+
+    public void RemoveScrapCount (int other) { _scrapCount -= other; }
+
+    public void SetScrapCount (int other) { _scrapCount = other; }
+}
