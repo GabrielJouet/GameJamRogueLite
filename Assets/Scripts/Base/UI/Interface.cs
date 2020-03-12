1991997e3dca71d64d57c07d7fb4268c897ee89c
DULEU LUCAS <lucas.duleu@viacesi.fr>
2020-03-12 14:55:36 +0100

    [UI] Spell Interface re-implemented

---
+++ Assets/Scripts/Base/UI/Interface.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interface : MonoBehaviour
+{
+    [SerializeField]
+    private Text _mainText;
+    [SerializeField]
+    private Text _boostText;
+    [SerializeField]
+    private Image _boostIcon;
+    [SerializeField]
+    private Text _upgradeText;
+
+    [SerializeField]
+    private GameObject _ui;
+
+    [SerializeField]
+    private Button _btn;
+
+    [SerializeField]
+    private string _type;
+
+    [SerializeField]
+    private List<Upgrade> _availableUpgrades;
+    private Upgrade _loadedUpgrade;
+
+    private TransitionSaver _transitionSaver;
+
+    private int _level = 0;
+
+
+
+    private void Start()
+    {
+        _transitionSaver = FindObjectOfType<TransitionSaver>();
+        _level = _transitionSaver.RecoverUpgradeBench(_type);
+        DisplayChange();
+    }
+
+
+    public void Upgrade()
+    {
+        if(_level < _availableUpgrades.Count)
+        {
+            if(_transitionSaver.GetScrapCount() >= _loadedUpgrade.GetCost())
+            {
+                _level++;
+                _transitionSaver.SetBenchUpgrade(_type);
+                DisplayChange();
+            }
+        }
+    }
+
+
+    private void DisplayChange()
+    {
+        _loadedUpgrade = _availableUpgrades[_level];
+        _mainText.text = _loadedUpgrade.GetDescriptionText();
+        _boostText.text = _loadedUpgrade.GetBoost().ToString();
+        _upgradeText.text = "Upgrade for " + _loadedUpgrade.GetCost();
+        _boostIcon.sprite = _loadedUpgrade.GetIconSprite();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _ui.SetActive(true);
+        DisplayChange();
+        _btn.onClick.RemoveAllListeners();
+        _btn.onClick.AddListener(delegate { this.Upgrade(); });
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _ui.SetActive(false);
+    }
+}
--- Assets/Scripts/Base/UI/InterfaceSpawner.cs
+++ Assets/Scripts/Base/UI/InterfaceSpawner.cs
@@ -4,36 +4,15 @@ using UnityEngine;
 
 public class InterfaceSpawner : MonoBehaviour
 {
-    private bool _canOpenInterface = false;
-    private bool _interfaceIsOpen = false;
-
     [SerializeField]
     private GameObject _interface;
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E) && _canOpenInterface && !_interfaceIsOpen)
-        {
-            _interface.SetActive(true);
-            _interfaceIsOpen = true;
-        }
-    }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
-        {
-            _canOpenInterface = true;
-        }
-        
+        _interface.SetActive(true);
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.tag == "Player")
-        {
-            _interface.SetActive(false);
-            _interfaceIsOpen = false;
-            _canOpenInterface = false;
-        }
+        _interface.SetActive(false);
     }
 }
