e93958a2fcf59839404af1ed1b347c75b9f12a96
DULEU LUCAS <lucas.duleu@viacesi.fr>
2020-03-13 14:54:18 +0100

    [UI] Player Health Bar
    - Added an adaptative health bar for the player in the dungeon scene

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private int _maxHealth = 20;
+    private int _currentHealth;
+
+    [SerializeField]
+    private HealthBarScript _healthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentHealth = _maxHealth;
+        _healthBar.SetMaxHealth(_maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) //to test the health bar
+        {
+            TakeDamage(1);
+        }
+    }
+
+    private void TakeDamage(int damage)
+    {
+        _currentHealth -= damage;
+        _healthBar.SetHealth(_currentHealth);
+    }
+}
